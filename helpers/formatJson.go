1bbe7a3dcef3a619690723be4666d8f621cb6f06
hugo emannuel <hugoemannuel53@gmail.com>
2024-08-13 18:15:08 -0300

    chore: Remove unused code in formatJson.go

--- helpers/formatJson.go
+++ helpers/formatJson.go
@@ -2,14 +2,9 @@ package helpers
 
 import (
 	"encoding/json"
-	"io"
 )
 
 func FormatJson(body []byte, jsonData interface{}) interface{} {
 	json.Unmarshal(body, &jsonData)
 	return jsonData
-}
-
-func FormatBody(body io.Reader, jsonData interface{}) error {
-	return json.NewDecoder(body).Decode(&jsonData)
 }
